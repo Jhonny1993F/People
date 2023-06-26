1bc36f0e47b0584250f63376b6f80f983c5ff82f
Jhonny Montero <jhonnymontero88@gmail.com>
2023-06-26 08:46:46 -0500

    Agregar archivos de proyecto.

---
+++ People/Modelos/Persona.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace People.Modelos
+{
+    [Table("people")]
+    public class Persona
+    {
+        //public int Id { get; set; }
+        //public string Name { get; set; }
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [MaxLength(250), Unique]
+        public string Name { get; set; }
+    }
+}
